f0d716c1c3d9adcd6a495bf3b4155fa8868c9798
andierodin@gmail.com <Thewayto1>
2019-01-29 03:11:08 +0300

    Add: main.cpp for using object-orient programing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+class Human
+{
+public:
+  int age ;
+  int weight ;
+  string name;
+};
 int main()
 {
-cout << "Hello World!" << endl;
-return 0;
+  Human firstHuman ;
+  Human secondHuman;
+  firstHuman.age =15;
+  firstHuman.name="Ivan Vasilievich";
+  firstHuman.weight=100;
+  secondHuman.age=30;
+  secondHuman.name="Andrey Urievich";
+  secondHuman.weight=45;
+
 }
