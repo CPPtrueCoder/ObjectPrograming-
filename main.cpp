77fb900e26544ea1a1b126cc401933927b032f86
andierodin@gmail.com <Thewayto1>
2019-01-30 04:13:21 +0300

    Add: Constructors , Setters and Getters for classes .

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 #include <string>
-using namespace std;
-class Human
-{
-public:
-  int age ;
-  int weight ;
-  string name;
-};
-int main()
-{
-  Human firstHuman ;
-  Human secondHuman;
-  firstHuman.age =15;
-  firstHuman.name="Ivan Vasilievich";
-  firstHuman.weight=100;
-  secondHuman.age=30;
-  secondHuman.name="Andrey Urievich";
-  secondHuman.weight=45;
 
+using namespace std;
+class Point {
+private:
+  int x;
+  int y;
+
+public:
+  Point(int valueX, int valueY) {
+    x = valueX;
+    y = valueY;
+  }
+  int GetY() { return y; }
+  void SetY(int valueY) { y = valueY; }
+  int GetX() { return x; }
+  void SetX(int valueX) { x = valueX; }
+  void Show() { cout << "X= " << x << "\t Y = " << y << endl << endl; }
+};
+class CoffeGrinder {
+private:
+  bool CheckVoltage() { return false; }
+
+public:
+  void Start() {
+
+    if (CheckVoltage()) {
+      cout << "Wjuuuh" << endl;
+
+    } else {
+      cout << "Beep Beep " << endl;
+    }
+  }
+};
+
+int main() {
+
+  Point a(5, 44);
+  a.Show();
+  Point b(77, 9);
+  b.Show();
 }
